1e30bfc308261e736537d8bbde9fb2c504c0fae0
unknown <you@example.com>
2024-05-28 21:43:29 +0600

    module-13.2

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -14,13 +14,31 @@ const findLastStudentId = async () => {
     .sort({ createdAt: -1 })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  //2030 01 0001
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // year, semester code, 4 degit number
 export const generateStudentId = async (payLoad: TAcademicSemester) => {
   // first time 0000
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString(); //0000 by default
+
+  const lastStudentId = await findLastStudentId();
+  //2030 01 0001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6); //01
+  const lastStudentYear = lastStudentId?.substring(0, 4); //2030
+
+  const currentSemesterCode = payLoad.code;
+  const currentYear = payLoad.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payLoad.year}${payLoad.code}${incrementId}`;
