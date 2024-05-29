72b3d97eea9be682e0614f8dcb63b7a18c670ded
unknown <you@example.com>
2024-05-29 08:58:46 +0600

    module-13.5

---
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -0,0 +1,38 @@
+import { TAcademicDepartment } from './academicDepartment.interface';
+import { AcademicDepartment } from './academicDepartment.model';
+
+const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
+  const result = await AcademicDepartment.find(payload);
+  return result;
+};
+
+const getAllAcademicdepartmentFromDB = async () => {
+  const result = await AcademicDepartment.find();
+  return result;
+};
+
+const getSingleAcademicDepartmentFromDB = async (id: string) => {
+  const result =
+    await AcademicDepartment.findById(id).populate('academicFaculty');
+  return result;
+};
+
+const updateAcademicDepartmentIntoDB = async (
+  id: string,
+  payload: Partial<TAcademicDepartment>,
+) => {
+  const result = await AcademicDepartment.findByIdAndUpdate(
+    { _id: id },
+    payload,
+    { new: true },
+  );
+
+  return result;
+};
+
+export const AcademicDepartmentServices = {
+  createAcademicDepartmentIntoDB,
+  getAllAcademicdepartmentFromDB,
+  getSingleAcademicDepartmentFromDB,
+  updateAcademicDepartmentIntoDB,
+};
