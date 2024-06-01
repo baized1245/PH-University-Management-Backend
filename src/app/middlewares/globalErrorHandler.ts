db600191e83b78c74f2a7836a4c7404f6ddee34d
unknown <you@example.com>
2024-06-01 19:47:59 +0600

    module-14.5

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,52 +2,55 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ErrorRequestHandler } from 'express';
-import { ZodError, ZodIssue } from 'zod';
-import { TErrorSource } from '../interface/error';
+import { ZodError } from 'zod';
+import { TErrorSources } from '../interface/error';
 import config from '../config';
+import handleZodError from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   // setting default values
   let statuscode = err.statusCode || 500;
   let message = err.message || 'Something went wrong!';
 
-  let errorSourses: TErrorSource = [
+  let errorSources: TErrorSources = [
     {
       path: '',
       message: 'Something went wrong!',
     },
   ];
 
-  const handleZodError = (err: ZodError) => {
-    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
-      return {
-        path: issue?.path[issue.path.length - 1],
-        message: issue.message,
-      };
-    });
-
-    const statuscode = 400;
-
-    return {
-      statuscode,
-      message: 'Validation Error',
-      errorSourses,
-    };
-  };
-
   if (err instanceof ZodError) {
     const simplifiedError = handleZodError(err);
-    statuscode = simplifiedError?.statuscode;
+    statuscode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
-    errorSourses = simplifiedError?.errorSourses;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'ValidatorError') {
+    const simplifiedError = handleValidationError(err);
+    statuscode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statuscode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statuscode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
 
   //ultimate return
   return res.status(statuscode).json({
     success: false,
     message,
-    errorSourses,
-    stack: config.NODE_ENVS === 'development' ? err?.stack : null,
+    errorSources,
+    err,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   });
 };
 
