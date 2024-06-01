f98fb4c1093b7f04fde0a6324f353c9546f6c808
unknown <you@example.com>
2024-06-01 20:42:51 +0600

    module-14.6

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,35 @@
+/* eslint-disable no-console */
+import { Server } from 'http';
 import app from './app';
 import config from './app/config';
 
 import mongoose from 'mongoose';
 
+let server: Server;
+
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
-      // eslint-disable-next-line no-console
+    server = app.listen(config.port, () => {
       console.log(`app listening on port ${config.port}`);
     });
   } catch (error) {
-    // eslint-disable-next-line no-console
     console.log(error);
   }
 }
 
 main();
+
+process.on('unhandledRejection', () => {
+  console.log('unhandledRejection is detected, shutting down...');
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+});
+
+process.on('uncaughtException', () => {
+  console.log('uncaughtException is detected, shutting down...');
+  process.exit(1);
+});
