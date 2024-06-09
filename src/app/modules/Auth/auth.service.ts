2b9dec8c1459e829f58125b1d79c578e4e4537ce
unknown <you@example.com>
2024-06-09 23:13:21 +0600

    module-17.5

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,8 +1,10 @@
 import bcrypt from 'bcrypt';
 import httpStatus from 'http-status';
+import jwt from 'jsonwebtoken';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.models';
 import { TLoginUser } from './auth.interface';
+import config from '../../config';
 
 const loginUser = async (payLoad: TLoginUser) => {
   //checking if the user is exist
@@ -15,27 +17,41 @@ const loginUser = async (payLoad: TLoginUser) => {
 
   // checking if the user is already deleted
 
-  //   const isDeleted = isUserExists?.isDeleted;
+  const isDeleted = user?.isDeleted;
 
-  //   if (isDeleted) {
-  //     throw new AppError(httpStatus.NOT_FOUND, 'This user is deleted!');
-  //   }
+  if (isDeleted) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is deleted!');
+  }
 
   // checking if the user is blocked
 
-  //   const userStatus = isUserExists?.status;
+  const userStatus = user?.status;
 
-  //   if (userStatus === 'blocked') {
-  //     throw new AppError(httpStatus.NOT_FOUND, 'This user is blocked!');
-  //   }
+  if (userStatus === 'blocked') {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is blocked!');
+  }
 
   //checking if the password is correct
 
   if (!(await User.isPasswordMatched(payLoad?.password, user?.password)))
+    // Access Granted: send AccessToken, RefreshToken
     throw new AppError(httpStatus.FORBIDDEN, 'Password do not matched');
 
-  // Access Granted: send AccessToken, RefreshToken
-  return {};
+  // create token and send o the client
+
+  const jwtPayload = {
+    userId: user.id,
+    role: user.role,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+    expiresIn: '90d',
+  });
+
+  return {
+    accessToken,
+    needsPasswordChange: user?.needsPasswordChange,
+  };
 };
 
 export const AuthServices = {
