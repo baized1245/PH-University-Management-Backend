e5e2e23d40bca8db66333e487fba2209b6e5d4d3
unknown <you@example.com>
2024-06-20 02:56:36 +0600

    module-19.4

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -8,16 +8,16 @@ export const sendEmail = async (to: string, html: string) => {
     secure: config.NODE_ENV === 'production',
     auth: {
       user: 'mdbaizedhasans@gmail.com',
-      pass: 'jn7jnAPss4f63QBp6D',
+      pass: 'inks zdex ecso vofm',
     },
   });
 
   // send mail with defined transport object
   await transporter.sendMail({
     from: 'mdbaizedhasans@gmail.com', // sender address
-    to: 'mdsadhin488@gmail.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world?', // plain text body
-    html: '<b>Hello world?</b>', // html body
+    to, // list of receivers
+    subject: 'Reset your password within 30 minutes', // Subject line
+    text: '', // plain text body
+    html, // html body
   });
 };
