28bcaf87500cbf8b2e2d0b165259de7440de9b41
unknown <you@example.com>
2024-06-20 12:38:56 +0600

    module-19.6

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -2,12 +2,17 @@ import express from 'express';
 import { StudentControllers } from './student.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { updateStudentValidationSchema } from './student.validation';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.get('/', StudentControllers.getAllStudent);
 
-router.get('/:id', StudentControllers.getSingleStudent);
+router.get(
+  '/:id',
+  auth('admin', 'faculty'),
+  StudentControllers.getSingleStudent,
+);
 
 router.patch(
   '/:id',
