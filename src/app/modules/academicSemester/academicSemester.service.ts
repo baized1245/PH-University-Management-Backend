180730cb29f73a50c0981ef6570e58ab60476d57
unknown <you@example.com>
2024-07-14 16:35:06 +0600

    Code updated

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,4 +1,5 @@
-import { academicSemesterCodeMapper } from './academicSemester.constant';
+import QueryBuilder from '../../builder/QueryBuilder';
+import { academicSemesterCodeMapper, AcademicSemesterSearchableFields } from './academicSemester.constant';
 import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
@@ -14,9 +15,21 @@ const createAcademicSemesterIntoDB = async (payLoad: TAcademicSemester) => {
   return result;
 };
 
-const getAllAcademicSemestersFromDB = async () => {
-  const result = await AcademicSemester.find();
-  return result;
+const getAllAcademicSemestersFromDB = async (query: Record<string, unknown>,) => {
+
+  const academicSemesterQuery = new QueryBuilder(AcademicSemester.find(), query)
+    .search(AcademicSemesterSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await academicSemesterQuery.modelQuery;
+  const meta = await academicSemesterQuery.countTotal();
+  return {
+    meta,
+    result,
+  };
 };
 
 const getSingleAcademicSemesterFromDb = async (id: string) => {
